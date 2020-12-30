c12679c1b83f729a28946de34a7166534668e2cc
felipe <felipemr698@gmail.com>
2020-12-30 17:40:57 -0300

    Refactored v5

---
+++ src/main/java/com/github/cc3002/finalreality/model/Controller/Phases/EnemyPhase.java
@@ -0,0 +1,12 @@
+package com.github.cc3002.finalreality.model.Controller.Phases;
+
+import com.github.cc3002.finalreality.model.Controller.ControllerFF;
+
+public class EnemyPhase extends Phase {
+    public EnemyPhase(ControllerFF controllerFF) {
+        super(controllerFF);
+    }
+
+    @Override
+    public void tryToEquip(String currentTurn, String weaponName){}
+}
